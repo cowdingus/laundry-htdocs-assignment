220bdf4ed2e8c1ddbc6dad76af312893e1e77e16
cowdingus <zaki.nadhif45@gmail.com>
2022-01-15 07:29:08 +0700

    Add conditionals in navbar

--- components/navbar.php
+++ components/navbar.php
@@ -1,6 +1,7 @@
 <?php
 function navbar()
 {
+	session_start();
 ?>
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
 		<div class="container-fluid">
@@ -13,6 +14,7 @@ function navbar()
 			</button>
 			<div class="collapse navbar-collapse" id="navbarSupportedContent">
 				<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+					<?php if ($_SESSION["role"] === "admin"): ?>
 					<li class="nav-item">
 						<a class="nav-link active" aria-current="page" href="/home.php">Home</a>
 					</li>
@@ -25,11 +27,46 @@ function navbar()
 					<li class="nav-item">
 						<a class="nav-link" href="/paket">Paket</a>
 					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="/transaksi">Transaksi</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="/laporan">Laporan</a>
+					</li>
+					<?php endif ?>
+					<?php if ($_SESSION["role"] === "kasir"): ?>
+					<li class="nav-item">
+						<a class="nav-link active" aria-current="page" href="/home.php">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="/member">Member</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="/transaksi">Transaksi</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="/laporan">Laporan</a>
+					</li>
+					<?php endif ?>
+					<?php if ($_SESSION["role"] === "owner"): ?>
+					<li class="nav-item">
+						<a class="nav-link active" aria-current="page" href="/home.php">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="/laporan">Laporan</a>
+					</li>
+					<?php endif ?>
 				</ul>
 				<ul class="navbar-nav mb-2 mb-lg-0">
+					<?php if (isset($_SESSION["id"])): ?>
 					<li class="nav-item">
 						<a class="nav-link" href="/logout.php">Logout</a>
 					</li>
+					<?php else: ?>
+					<li class="nav-item">
+						<a class="nav-link" href="/login.php">Login</a>
+					</li>
+					<?php endif ?>
 				</ul>
 			</div>
 		</div>
