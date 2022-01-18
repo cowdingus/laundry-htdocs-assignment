a4926f8633d109ade535eb22ec3b3308f71245f8
cowdingus <zaki.nadhif45@gmail.com>
2022-01-18 10:45:12 +0700

    Turn status into badge in tampil transaksi

--- transaksi/index.php
+++ transaksi/index.php
@@ -5,8 +5,10 @@ allow_page_access_exclusive(["admin", "kasir"]);
 
 require_once "../components/bootstrap.php";
 require_once "../components/navbar.php";
+require_once "../utility/utils.php";
 
-function extract_member_name_from_id($id) {
+function extract_member_name_from_id($id)
+{
 	global $conn;
 
 	$query = mysqli_query($conn, "SELECT nama FROM member WHERE id = $id");
@@ -16,7 +18,8 @@ function extract_member_name_from_id($id) {
 	return $data_member["nama"];
 }
 
-function extract_user_name_from_id($id) {
+function extract_user_name_from_id($id)
+{
 	global $conn;
 
 	$query = mysqli_query($conn, "SELECT nama FROM user WHERE id = $id");
@@ -24,6 +27,24 @@ function extract_user_name_from_id($id) {
 	$data_member = mysqli_fetch_assoc($query);
 
 	return $data_member["nama"];
+}
+
+function status_to_badge_converter($status)
+{
+	$bg_theme = "bg-primary";
+
+	switch ($status) {
+		case "dibayar":
+			$bg_theme = "bg-success";
+			break;
+		case "belum_dibayar":
+			$bg_theme = "bg-warning text-dark";
+			break;
+	}
+
+	$status = titleize($status);
+
+	return "<span class=\"badge rounded-fill $bg_theme\">$status</span>";
 }
 
 ?>
@@ -55,7 +76,7 @@ function extract_user_name_from_id($id) {
 
 		$query = mysqli_query($conn, "SELECT t.id, m.nama as nama_member, t.tgl, t.batas_waktu, t.tgl_bayar, t.status, t.dibayar, u.nama as nama_kasir, p.jenis as paket, p.harga * d_t.qty as total FROM transaksi t, detail_transaksi d_t, paket p, member m, user u WHERE t.id_member = m.id AND t.id_user = u.id AND t.id = d_t.id_transaksi AND p.id = d_t.id_paket");
 
-		list_table($query);
+		list_table($query, ["status" => "status_to_badge_converter", "dibayar" => "status_to_badge_converter"]);
 		?>
 
 	</main>
